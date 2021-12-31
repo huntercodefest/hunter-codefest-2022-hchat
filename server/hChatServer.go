dc915fc31ce3987c59f8d5416ad158bc1635190a
Daniel Volchek <Danielvolchek123@gmail.com>
2021-12-31 20:50:38 +0000

    commit

--- server/hChatServer.go
+++ server/hChatServer.go
@@ -8,6 +8,7 @@ const (
 	TYPE = "tcp"
 	HOST = "localhost"
 	PORT = "4444"
+	_LEN = 1024 // Max length of message
 )
 
 func main() {
@@ -16,12 +17,27 @@ func main() {
 
 func startServer() {
 	// Listen is server variable
-	listen, err := net.Listen(TYPE, HOST+":"+PORT)
+	server, err := net.Listen(TYPE, HOST+":"+PORT)
 	// If server is not initialized correctly
 	if err != nil {
 		ExitOnErr(err)
 	}
-	// placeholder
-	listen.Accept()
+	// Main loop to handle accepting connections
+	for {
+		conn, err := server.Accept()
+		// If connection fails
+		if err != nil {
+			continue
+		}
+		// Seperates connection handle function from loop to allow main server to continue allowing new connections
+		go processInitialConnection(conn)
+	}
+}
 
+// Run as seperate Goroutine
+func processInitialConnection(conn net.Conn) {
+	// func first needs to process message
+	// then validate username and room connection
+	// then seperate user into appropriate room
+	// then open new goroutine to read for new incoming messages from user to distribute to room
 }
--- server/user.go
+++ server/user.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"net"
 )
 
@@ -23,4 +24,50 @@ func NewUser(user_con net.Conn, username string) (*User, error) {
 //  Function to guarantee correct username format, may be better to do on client side
 func ValidateUsername(username string) (valid bool) {
 	return
+}
+
+func ReadConnForNewMessage(user User) (message string) {
+	// infinite loop conditional
+	conn_failed := false
+	for !conn_failed {
+		// Buffer slice to contain sent message
+		msgbuf := make([]byte, _LEN)
+		_, err := user.user_con.Read(msgbuf)
+		if err != nil {
+			conn_failed = true
+		}
+		// Buffer requires processing due to extra 0 bits after message
+		var input string
+		for index, element := range msgbuf {
+			if element == 0 {
+				// slice grabbing all elements of array before index
+				input = string(msgbuf[:index])
+				break
+			}
+		}
+		room_num, username, err := ProcessInput(input)
+		// placeholder
+		fmt.Println(room_num, username, err)
+	}
+	// placeholder
+	return
+}
+
+// func to process message
+func ProcessInput(input string) (room_number int, username string, err error) {
+	if input[0] != '#' {
+		return 0, "", errors.New("message must be prefixed with #")
+	}
+	// placeholder
+	return
+	/*
+		Message Syntax:
+		#00000 - To indicate room
+		_username - To indicate user
+		:message - To indicate message
+		<----------------------------------------------->
+		The full syntax should look something like this:
+		#00000_username:message
+		#00000_username with no message can indicate entering and exiting chatroom
+	*/
 }
