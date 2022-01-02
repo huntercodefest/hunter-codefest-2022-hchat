6f312775f1c63a27a234eb4355ea7fa758552a01
Daniel Volchek <Danielvolchek123@gmail.com>
2022-01-02 00:45:29 +0000

    Improved background script, added conditional execution

--- server/bgserver.sh
+++ server/bgserver.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-SERVER="go/server/server.go"
+SERVER_PATH="$HOME/go/hunter-codefest-2022-hchat/server/hChatServer.go"
+NGROK_PATH="$HOME/ngrok"
 PORT=8888
-if [[ -f "$HOME/$SERVER" ]] && [[ -f "$HOME/ngrok" ]]; then
+if [[ -f "$SERVER_PATH" ]] && [[ -f "$NGROK_PATH" ]]; then
 	 nohup go run $HOME/$SERVER > /dev/null &
 	 echo "SERVER: $!" > ps.txt
-	 nohup ./ngrok tcp $PORT > /dev/null &
+	 nohup $NGROK_PATH tcp $PORT > /dev/null &
 	 echo "NGROK: $!" >> ps.txt 
 	 echo "Started Procceses"
 	 ps=`cat ps.txt`
 	 echo -e "\e[1;34m$ps\e[1;m"
 	 echo -e "\e[1;31mPlease run command alias ngrokaddr for address\e[1;m"
+else
+	if [[ ! -f "$SERVER_PATH" ]]; then
+		 echo "Server path is wrong"
+	 fi
+	 if [[ ! -f "$NGROK_PATH" ]]; then
+		 echo "Ngrok path is wrong"
+	 fi
 fi
 
