252bda7cc4e0a8b105899fb80b982727f62bfcf3
DanielVolchek <danielvolchek123@gmail.com>
2022-01-04 23:06:49 +0000

    added response functions

--- server/helper_funcs.go
+++ server/helper_funcs.go
@@ -30,7 +30,7 @@ func ReadConnOnLoop(p_usr_conn *net.Conn) (err error) {
 			break
 			// TODO change to remove user connection from room if read fails (check err type to not be message failed to read)
 		}
-		room_num, username, message, err := ProcessInput(&msgbuf)
+		room_num, username, message, err := ProcessInput(msgbuf)
 		// placeholder
 		fmt.Println(room_num, username, message, err)
 	}
@@ -49,19 +49,20 @@ func ReadConnOnLoop(p_usr_conn *net.Conn) (err error) {
 	#00000_username:message
 	#00000_username with no message can indicate entering and exiting chatroom
 */
-func ProcessInput(msgbuf *[]byte) (room_number int, username string, message string, err error) {
+func ProcessInput(msgbuf []byte) (room_number int, username string, message string, err error) {
 	var input string
 	// Buffer requires processing due to extra 0 bits after message
 	foundBuffEnd := false
-	for index, element := range *msgbuf {
+	for index, element := range msgbuf {
 		if element == 0 {
 			// slice grabbing all elements of array before index
-			input = string((*msgbuf)[:index])
+			input = string((msgbuf)[:index])
 			foundBuffEnd = true
 		}
 	}
+	// If passed in message is max length and no zeros are found
 	if !foundBuffEnd {
-		input = string(*msgbuf)
+		input = string(msgbuf)
 	}
 	// if input does not contain #, _, :
 	if input[0] != '#' || !strings.ContainsRune(input, '_') || !strings.ContainsRune(input, ':') {
@@ -82,4 +83,22 @@ func ProcessInput(msgbuf *[]byte) (room_number int, username string, message str
 	}
 	// placeholder
 	return 0, "", "", errors.New("wrong format, please consult github readme")
+}
+
+// Suite of respond to client functions
+
+// Overarching response function called by primary use respond functions
+// Generally should not be used but is available in the case it is needed
+func RespondToClient(p_user *User, msgbuf []byte) (err error) {
+	conn := (*p_user).user_con
+	_, err = conn.Write(msgbuf)
+	return err
+}
+
+func RespondWithString(p_user *User, message string) (err error) {
+	return RespondToClient(p_user, []byte(message))
+}
+
+func RespondWithErr(p_user *User, passed_err error) (err error) {
+	return RespondToClient(p_user, []byte(passed_err.Error()))
 }
