6430e0150eb682670e78aa5d96e0b240767da7f0
danielvolchek <danielvolchek123@gmail.com>
2022-01-11 02:28:45 -0500

    began adding websocket protocol functionality

--- server/hChatServer.go
+++ server/hChatServer.go
@@ -20,16 +20,19 @@ func main() {
 	// Use wait group to keep program running
 	var wg sync.WaitGroup
 	wg.Add(2)
-	// Websocket server code
-	go func() {
-		setupRoutes()
-		fmt.Println("WS SERVER FIN")
-		wg.Done()
-	}()
 	// TCP server code
+	fmt.Println("different")
 	go func() {
+		fmt.Println("Do this first")
 		startServer()
 		fmt.Println("TCP SERVER FIN")
+		wg.Done()
+	}()
+	// Websocket server code
+	go func() {
+		fmt.Println("Do this second")
+		setupHTTPRoutes()
+		fmt.Println("WS SERVER FIN")
 		wg.Done()
 	}()
 	// servers should not hit this spot unless something goes wrong
@@ -53,15 +56,15 @@ func startServer() {
 			continue
 		}
 		// Seperates connection handle function from loop to allow main server to continue allowing new connections
-		go processInitialConnection(conn)
+		go processInitialConnection(&conn)
 	}
 }
 
 // Run as seperate Goroutine
-func processInitialConnection(conn net.Conn) (err error) {
+func processInitialConnection(conn *net.Conn) (err error) {
 	// func first needs to process message
-	fmt.Println("New client connected: " + conn.RemoteAddr().String())
-	msgbuf, err := ReadSingleMessage(&conn)
+	fmt.Println("New client connected: " + (*conn).RemoteAddr().String())
+	msgbuf, err := ReadSingleMessage(conn)
 	fmt.Println("passed read single message")
 	if err != nil {
 		return err
@@ -70,14 +73,14 @@ func processInitialConnection(conn net.Conn) (err error) {
 	room_num, username, _, err := ProcessInput(msgbuf)
 	fmt.Println("passed process input")
 	if err != nil {
-		conn.Write([]byte(err.Error()))
+		(*conn).Write([]byte(err.Error()))
 		return
 	}
 	// Validate username and room existence
 	fmt.Println("created new user")
 	p_user, err := NewUser(conn, username, ROOM_MAP)
 	if err != nil {
-		conn.Write([]byte(err.Error()))
+		(*conn).Write([]byte(err.Error()))
 		return
 	}
 	if _, ok := ROOM_MAP[room_num]; !ok {
--- server/websocket_server.go
+++ server/websocket_server.go
@@ -4,23 +4,61 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+/*
+This file is a very basic first version of a websocket server
+It is a one to one clone of a video I found on https://tutorialedge.net/golang/go-websocket-tutorial/
+Down the line I will adapt it to fit our needs
+*/
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  _LEN,
+	WriteBufferSize: _LEN,
+}
+
+func reader(conn *websocket.Conn) {
+	// TODO
+	// Find out if ReadMessage and WriteMessage are interchangable with Read and Write
+
+	// If theres no function to write to either use switch to check if type is of websocket or of tcp connection
+	// After that forget about this function entirely and adapt to processRoutine function
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println(string(p))
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Home page")
 }
 
 func wsEndpoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Go websockets")
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("Client succesfully connected...")
+	reader(ws)
+
 }
 
-func setupRoutes() {
+func setupHTTPRoutes() {
 	fmt.Println("Setting up routes")
 	http.HandleFunc("/", homePage)
 	http.HandleFunc("/ws", wsEndpoint)
 	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func startWSServer() {
-	setupRoutes()
 }
