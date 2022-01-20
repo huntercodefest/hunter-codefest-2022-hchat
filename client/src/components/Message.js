85ab25dfff48f14f606aa14ce4e6fd0af1edc82c
danielvolchek <danielvolchek123@gmail.com>
2022-01-20 01:38:03 -0500

    lots of changes

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -4,18 +4,24 @@ class Message extends React.Component {
 	constructor(props) {
 		super(props);
 		this.time = {
-			hours: props.time.getHours(),
+			hours: props.time.getHours() % 12,
 			minutes: props.time.getMinutes(),
 		};
-        this.message = {
-            username: props.username,
-            message: props.message,
-        }
+		this.message = {
+			username: props.username,
+			message: props.message,
+		};
 	}
 	render() {
 		return (
-        <p>{this.time.hours}:{this.time.minutes}</p>
-        );
+			<span>
+				<p>{this.props.username}</p>
+				<p>{this.props.message}</p>
+				<p>
+					{this.time.hours}:{this.time.minutes}
+				</p>
+			</span>
+		);
 	}
 }
 
