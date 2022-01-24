1da9666368a2271f5096da5ffb530f1fb6782eac
DanielVolchek <danielvolchek123@gmail.com>
2022-01-24 08:13:40 +0000

    moved files and changed names

---
+++ webapp/servehome.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'home-site')));
+
+// homepage
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'home-site', 'index.html'));
+});
+
+
+console.log("Listening on port 3000")
+app.listen(3000);
