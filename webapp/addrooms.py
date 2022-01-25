fc1e4d72839bd055163edf6a90174ee01cc994cb
danielvolchek <danielvolchek123@gmail.com>
2022-01-25 02:42:15 -0500

    modified comments in addrooms.py

---
+++ webapp/addrooms.py
@@ -0,0 +1,24 @@
+# highest num is 86597
+# need to load all directories into list
+# then append to every major json per directory
+# add new object to each one
+""" 
+major: major
+room_num: count+1
+room_desc: major
+"""
+# create new json in every directory with one object filename school.json
+""" 
+major: school
+room_num: count+1
+room_desc: school
+ """
+# modify _PATHS json in each dir to contain the new paths to those jsons
+
+# in react
+# modify code somewhere to read new rooms as chatrooms instead of lists
+# modify roomlist component to give different class for school/major rooms
+# set school/major rooms to first element with flex property order: 1;
+# modify roomlist component to give chatrooms bold font weight
+
+
